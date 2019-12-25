517eca96ebdf86357a44a22293da7d2c7eb9fc7c
Kudusch <kudusch@posteo.de>
2019-12-25 22:36:04 +0100

    Add styling and timer

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+    /* Disable zoom gestures */
+    document.addEventListener('touchmove', function (event) {
+        if (event.scale !== 1) { event.preventDefault(); }
+    }, false);
+    var i = 0;
+    document.addEventListener('touchend', function (event) {
+        var j = (new Date()).getTime();
+        if (j - i <= 100) {
+            event.preventDefault();
+        }
+        i = j;
+    }, false);
+
     var d2 = [];
     for (var i = 1; i != 3; ++i) d2.push(i)
     var d4 = [];
@@ -71,14 +84,14 @@ $(document).ready(function() {
             count = $(this).find(".count").text();
             if (count > 0) {
                 die = $(this).attr("id");
-                modifier_value = $(this).find(".modifier-value").find("input").val();
-                modifier_value = (modifier_value<=0?"":"+") + modifier_value;
+                modifier_value = $(this).find(".mod-value").text().replace("±", "");
                 modifier_value = (modifier_value=="0"?"":modifier_value);
                 lines = lines + "<p>" + count + die + modifier_value + "</p>";
                 dice[die] = {"count":count, modifier:modifier_value}
             }
             $("#dice_output").html(lines);
             if (lines == "") {
+                $("#dice_output").text("No dice selected");
                 $("#roll_btn").addClass("half");
             } else {
                 $("#roll_btn").removeClass("half");
@@ -92,6 +105,10 @@ $(document).ready(function() {
     $(".plus").click(function() {
         var c = Number($(this).closest("tr").find(".count").text());
         $(this).closest("tr").find(".count").text(String(c + 1));
+
+        $(this).closest("tr").find(".mod-minus").removeClass("half");
+        $(this).closest("tr").find(".mod-plus").removeClass("half");
+
         prepare_dice();
     });
     $(".minus").click(function() {
@@ -99,30 +116,85 @@ $(document).ready(function() {
         if (c > 0) {
             $(this).closest("tr").find(".count").text(String(c - 1));
         }
+        if ((c - 1) > 0) {
+            $(this).closest("tr").find(".mod-minus").removeClass("half");
+            $(this).closest("tr").find(".mod-plus").removeClass("half");
+        } else {
+            $(this).closest("tr").find(".mod-minus").addClass("half");
+            $(this).closest("tr").find(".mod-plus").addClass("half");
+            $(this).closest("tr").find(".mod-value").addClass("half");
+            $(this).closest("tr").find(".mod-value").removeClass("positive");
+            $(this).closest("tr").find(".mod-value").removeClass("negative");
+            $(this).closest("tr").find(".mod-value").text("±" + 0);
+        }
         prepare_dice();
     });
 
-    $(".modifier-value").click(function() {
-        if ($(this).find("input").val() == 0) {
-            $(this).addClass("half");
-        } else {
-            $(this).removeClass("half");
+    $(".mod-plus").click(function() {
+        if (($(this).closest("tr").find(".count").text()) > 0) {
+            var c = Number($(this).closest("tr").find(".mod-value").text().replace("±", ""));
+            c = c + 1
+            c = (c<=0?"":"+") + c;
+            $(this).closest("tr").find(".mod-value").text(c);
+            if (c == 0) {
+                $(this).closest("tr").find(".mod-value").addClass("half");
+                $(this).closest("tr").find(".mod-value").removeClass("positive");
+                $(this).closest("tr").find(".mod-value").removeClass("negative");
+                $(this).closest("tr").find(".mod-value").text("±" + c);
+            } else {
+                $(this).closest("tr").find(".mod-value").removeClass("half");
+                if (c > 0) {
+                    $(this).closest("tr").find(".mod-value").removeClass("negative");
+                    $(this).closest("tr").find(".mod-value").addClass("positive");
+                } else if (c < 0) {
+                    $(this).closest("tr").find(".mod-value").removeClass("positive");
+                    $(this).closest("tr").find(".mod-value").addClass("negative");
+                }
+            }
+        }
+        prepare_dice();
+    });
+    $(".mod-minus").click(function() {
+        if (($(this).closest("tr").find(".count").text()) > 0) {
+            var c = Number($(this).closest("tr").find(".mod-value").text().replace("±", ""))
+            c = c - 1
+            c = (c<=0?"":"+") + c;
+            $(this).closest("tr").find(".mod-value").text(c);
+            if (c == 0) {
+                $(this).closest("tr").find(".mod-value").addClass("half");
+                $(this).closest("tr").find(".mod-value").removeClass("positive");
+                $(this).closest("tr").find(".mod-value").removeClass("negative");
+                $(this).closest("tr").find(".mod-value").text("±" + c);
+            } else {
+                $(this).closest("tr").find(".mod-value").removeClass("half");
+                if (c > 0) {
+                    $(this).closest("tr").find(".mod-value").removeClass("negative");
+                    $(this).closest("tr").find(".mod-value").addClass("positive");
+                } else if (c < 0) {
+                    $(this).closest("tr").find(".mod-value").removeClass("positive");
+                    $(this).closest("tr").find(".mod-value").addClass("negative");
+                }
+            }
         }
         prepare_dice();
     });
 
     $("#roll_btn").click(function() {
-        dice = prepare_dice();
-        $("#roll_output").html("");
-        var lines = "";
-        var roll_total = 0;
-        for (var i in dice) {
-            roll = dice_roll(i, dice[i]["count"]);
-            roll_sum = parseInt(sum(roll));
-            roll_total += roll_sum + parseInt((dice[i]["modifier"]==""?0:dice[i]["modifier"]));
-            roll = roll.map(function style_roll(n) {if (n == max_roll(i)) {return("<span class='max'>" + n + "</span>");} else if (n == 1) {return("<span class='min'>" + n + "</span>");} else {return(n);}})
-            lines += "<p>" + i + ": " + roll.join(", ") + " = " + roll_sum + dice[i]["modifier"] + "</p>"
+        if (!$(this).hasClass("half")) {
+            dice = prepare_dice();
+            $("#roll_output").html("");
+            var lines = "";
+            var roll_total = 0;
+            for (var i in dice) {
+                roll = dice_roll(i, dice[i]["count"]);
+                roll_sum = parseInt(sum(roll));
+                roll_total += roll_sum + parseInt((dice[i]["modifier"]==""?0:dice[i]["modifier"]));
+                roll = roll.map(function style_roll(n) {if (n == max_roll(i)) {return("<span class='max'>" + n + "</span>");} else if (n == 1) {return("<span class='min'>" + n + "</span>");} else {return(n);}})
+                lines += "<p>" + i + ": " + roll.join(", ") + " = " + roll_sum + dice[i]["modifier"] + "</p>"
+            }
+            $("#roll_output").html(lines + "<p>---<br>" + roll_total + "</p>");
         }
-        $("#roll_output").html(lines + "<p>" + roll_total + "</p>");
+        $(this).addClass("half");
+        setTimeout(function() { $("#roll_btn").removeClass("half"); }, 3 * 1000);
     });
 });
